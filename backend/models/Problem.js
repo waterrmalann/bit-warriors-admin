fb0ec901b773dd8e564c0ffcd877fc6f7cab61e7
Alan <56948680+waterrmalann@users.noreply.github.com>
2023-11-22 04:07:43 +0530

    Mongoose models

---
+++ backend/models/Problem.js
@@ -0,0 +1,52 @@
+import mongoose from 'mongoose';
+
+const problemSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    difficulty: {
+        type: String,
+        enum: ['Easy', 'Medium', 'Hard'],
+        required: true
+    },
+    constraints: {
+        type: String,
+        required: true
+    },
+    examples: {
+        type: String,
+        required: true
+    },
+    tags: {
+        type: [String],
+        required: true
+    },
+    hint: {
+        type: String
+    },
+    upvotes: {
+        type: Number,
+        default: 0
+    },
+    downvotes: {
+        type: Number,
+        default: 0
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    modifiedAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Problem = mongoose.model('Problem', problemSchema);
+
+export default Problem;
