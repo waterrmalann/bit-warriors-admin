2ddc7d1af007f725df30b538c9042020098de2b2
Alan <56948680+waterrmalann@users.noreply.github.com>
2023-12-01 01:08:23 +0530

    fix(status): make overview work

--- backend/controllers/overviewController.js
+++ backend/controllers/overviewController.js
@@ -1,13 +1,12 @@
 import asyncHandler from 'express-async-handler';
 import dotenv from 'dotenv';
-import humanizePkg from 'humanize-duration';
-const { humanizeDuration } = humanizePkg;
+import humanizeDuration from 'humanize-duration';
 dotenv.config();
 
 // todo: Need a service discovery.
 const services = {
     "User Service": process.env.USER_SERVICE,
-    "Problem Service": process.env.PROBLEM_SERVICE
+    "Problem Service": process.env.PROBLEMS_SERVICE
 }
 
 const GetStatus = asyncHandler(async (req, res) => {
@@ -15,22 +14,27 @@ const GetStatus = asyncHandler(async (req, res) => {
     for (let service in services) {
         try {
             let startTime = Date.now();
-            const response = await fetch(services[service] + '/status');
+            let response;
+            try {
+                response = await fetch(services[service] + '/status');
+            } catch (err) {
+                response = { ok: false };
+            }
             let endTime = Date.now();
             const ping = humanizeDuration(endTime - startTime);
             if (!response.ok) {
                 statuses.push({ name: service, online: false, uptime: 'offline', environment: 'null', ping: ping });
             } else {
                 const data = await response.json();
-                const uptime = humanizeDuration(data.uptime * 1000);
+                const uptime = humanizeDuration(Math.floor(data.uptime * 1000));
                 statuses.push({ name: service, online: true, uptime: uptime, environment: data.environment, ping: ping });
             }
-            res.status(200).send(statuses);
         } catch (err) {
             console.error(err);
-            return res.status(500).send({ message: err.message });
+            statuses.push({ name: service, online: false, uptime: 'offline', environment: 'null', ping: ping });
         }
     }
+    res.status(200).send(statuses);
 });
 
 export default { GetStatus };
